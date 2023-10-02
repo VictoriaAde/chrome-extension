86ef5611f07adabc5ae3aecbc23640da646e258f
Victoria <adedayovicky123@gmail.com>
2023-10-02 10:43:14 +0100

    permission popup - done

--- popup/popup.js
+++ popup/popup.js
@@ -9,11 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
     if (toggleSwitch1.checked) {
       toggleSwitchLabel1.style.backgroundColor = "#120b48";
       toggleSwitchLabel1.style.borderColor = "#120b48";
-      toggleSwitchInner1.style.transform = "translateX(100%)";
+      toggleSwitchInner1.style.transform = "translateX(0)";
     } else {
       toggleSwitchLabel1.style.backgroundColor = "#120b48";
       toggleSwitchLabel1.style.borderColor = "#120b48";
-      toggleSwitchInner1.style.transform = "translateX(0)";
+      toggleSwitchInner1.style.transform = "translateX(100%)";
     }
   });
 
@@ -27,11 +27,76 @@ document.addEventListener("DOMContentLoaded", function () {
     if (toggleSwitch2.checked) {
       toggleSwitchLabel2.style.backgroundColor = "#120b48";
       toggleSwitchLabel2.style.borderColor = "#120b48";
-      toggleSwitchInner2.style.transform = "translateX(100%)";
+      toggleSwitchInner2.style.transform = "translateX(0)";
     } else {
       toggleSwitchLabel2.style.backgroundColor = "#120b48";
       toggleSwitchLabel2.style.borderColor = "#120b48";
-      toggleSwitchInner2.style.transform = "translateX(0)";
+      toggleSwitchInner2.style.transform = "translateX(100%)";
     }
   });
 });
+
+// Start Recording Button Click Event
+const startRecordingButton = document.querySelector(".start_btn");
+startRecordingButton.addEventListener("click", () => {
+  // alert("Start Recording button clicked");
+
+  // Create a new tab for the custom notification
+  chrome.tabs.create({
+    url: chrome.runtime.getURL(
+      "permission_notification/permission_notification.html"
+    ),
+  });
+});
+// // Start Recording Button Click Event
+// const startRecordingButton = document.querySelector(".start_btn");
+// startRecordingButton.addEventListener("click", async () => {
+//   try {
+//     // Check if screen recording is supported
+//     if (navigator.mediaDevices.getDisplayMedia) {
+//       const stream = await navigator.mediaDevices.getDisplayMedia({
+//         video: true,
+//       });
+
+//       // You can start screen recording with the 'stream' here
+
+//       // Display a notification asking for permission to record the screen
+//       const notificationOptions = {
+//         type: "basic",
+//         title: "Screen Recording Permission",
+//         message: "Do you want to allow this extension to record your screen?",
+//         iconUrl: "../assets/icon.png", // Replace with your extension's icon
+//         buttons: [
+//           { title: "Allow", iconUrl: "../assets/allow.png" }, // Replace with your own icons
+//           { title: "Block", iconUrl: "../assets/block.png" }, // Replace with your own icons
+//         ],
+//       };
+
+//       chrome.notifications.create(
+//         "screenRecordingPermission",
+//         notificationOptions,
+//         function (notificationId) {
+//           // Handle button click events from the notification
+//           chrome.notifications.onButtonClicked.addListener(function (
+//             clickedNotificationId,
+//             buttonIndex
+//           ) {
+//             if (clickedNotificationId === notificationId) {
+//               if (buttonIndex === 0) {
+//                 // User clicked "Allow," you can proceed with screen recording
+//               } else if (buttonIndex === 1) {
+//                 // User clicked "Block," handle accordingly
+//               }
+//               // Close the notification
+//               chrome.notifications.clear(notificationId);
+//             }
+//           });
+//         }
+//       );
+//     } else {
+//       alert("Screen recording is not supported in this browser.");
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
